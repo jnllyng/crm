175ce425b894699fa03ba077883d2434b9b43e50
jnllyng <jueunyang00@gmail.com>
2026-03-15 22:09:21 -0500

    Fix asset pipeline configuration

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_tree ../stylesheets .css
+//= link active_admin.css
+//= link active_admin.js
+//= link app.css
+//= link application.css
+//= link_tree ../../javascript .js
